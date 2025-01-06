c67e8b355e2c9847ee1d6a8250a64e75b5085f1a
DKanatnikov <dmitry.kanatnikov@ligastavok.ru>
2025-01-06 19:37:44 +0300

    HW-91 logs

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/AllureBackendLogsExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/AllureBackendLogsExtension.java
@@ -5,32 +5,41 @@ import io.qameta.allure.AllureLifecycle;
 import io.qameta.allure.model.TestResult;
 import lombok.SneakyThrows;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import static java.nio.file.Files.newInputStream;
+
 public class AllureBackendLogsExtension implements SuiteExtension {
 
-  public static final String caseName = "Niffler backend logs";
+    public static final String caseName = "Niffler backend logs";
 
-  @SneakyThrows
-  @Override
-  public void afterSuite() {
-    final AllureLifecycle allureLifecycle = Allure.getLifecycle();
-    final String caseId = UUID.randomUUID().toString();
-    allureLifecycle.scheduleTestCase(new TestResult().setUuid(caseId).setName(caseName));
-    allureLifecycle.startTestCase(caseId);
 
-    allureLifecycle.addAttachment(
-        "Niffler-auth log",
-        "text/html",
-        ".log",
-        Files.newInputStream(
-            Path.of("./logs/niffler-auth/app.log")
-        )
-    );
+    @Override
+    public void afterSuite() {
+        final AllureLifecycle allureLifecycle = Allure.getLifecycle();
+        final String caseId = UUID.randomUUID()
+                                  .toString();
+        allureLifecycle.scheduleTestCase(new TestResult().setUuid(caseId)
+                                                         .setName(caseName));
+        allureLifecycle.startTestCase(caseId);
 
-    allureLifecycle.stopTestCase(caseId);
-    allureLifecycle.writeTestCase(caseId);
-  }
+        addAllServicesLogAttachment();
+
+        allureLifecycle.stopTestCase(caseId);
+        allureLifecycle.writeTestCase(caseId);
+    }
+
+    @SneakyThrows
+    private void addAllServicesLogAttachment() {
+        final AllureLifecycle allureLifecycle = Allure.getLifecycle();
+        final String type = "text/html";
+        final String fileExtension = ".log";
+
+        allureLifecycle.addAttachment("Niffler auth log", type, fileExtension, newInputStream(Path.of("./logs/niffler-auth/app.log")));
+        allureLifecycle.addAttachment("Niffler currency log", type, fileExtension, newInputStream(Path.of("./logs/niffler-currency/app.log")));
+        allureLifecycle.addAttachment("Niffler-gateway log", type, fileExtension, newInputStream(Path.of("./logs/niffler-gateway/app.log")));
+        allureLifecycle.addAttachment("Niffler-spend log", type, fileExtension, newInputStream(Path.of("./logs/niffler-spend/app.log")));
+        allureLifecycle.addAttachment("Niffler-userdata log", type, fileExtension, newInputStream(Path.of("./logs/niffler-userdata/app.log")));
+    }
 }
